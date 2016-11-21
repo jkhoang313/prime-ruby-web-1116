6cc1ae7c7f15ce42faae5823ceab3a9d5f3492af
Jackson Hoang <jkhoang313@gmail.com>
2016-11-21 06:41:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  prime = true
+  divisors = (2..number/2).to_a
+  if number < 3
+    prime = false
+  else
+    divisors.each do |divisor|
+      if number % divisor == 0
+        prime = false
+      end
+    end
+  end
+  prime
+end
